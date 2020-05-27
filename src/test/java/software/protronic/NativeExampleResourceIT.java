39bfec0a018e85a34ea5e35f0dca82e02dc13627
quarkusio <no-reply@quarkus.io>
2020-05-27 10:32:35 +0000

    Initial commit

---
+++ src/test/java/software/protronic/NativeExampleResourceIT.java
@@ -0,0 +1,9 @@
+package software.protronic;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeExampleResourceIT extends ExampleResourceTest {
+
+    // Execute the same tests but in native mode.
+}
